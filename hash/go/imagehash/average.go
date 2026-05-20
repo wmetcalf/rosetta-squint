591a2209d9018d12db6a9a100f7f8baf8130bf1e
Will Metcalf <william.metcalf@gmail.com>
2026-05-20 10:29:21 -0500

    go: AverageHash compute + Group-2 goldens test (21 fixtures x 3 sizes)

---
+++ hash/go/imagehash/average.go
@@ -0,0 +1,50 @@
+package imagehash
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/wmetcalf/rosetta-image-hash/go/imagehash/internal/imgrgb"
+	"github.com/wmetcalf/rosetta-image-hash/go/imagehash/internal/lanczos"
+	"github.com/wmetcalf/rosetta-image-hash/go/imagehash/internal/pilgray"
+)
+
+func AverageHash(img image.Image, hashSize int) (Hash, error) {
+	if hashSize < 2 {
+		return Hash{}, fmt.Errorf("hashSize must be >= 2, got %d", hashSize)
+	}
+	rgb := imgrgb.ToRGB(img)
+	gray := rgbToGray(rgb)
+	resized := lanczos.Resize(gray, hashSize, hashSize)
+
+	var sum int64
+	for y := 0; y < hashSize; y++ {
+		for x := 0; x < hashSize; x++ {
+			sum += int64(resized[y][x])
+		}
+	}
+	avg := float64(sum) / float64(hashSize*hashSize)
+
+	bits := make([][]bool, hashSize)
+	for y := 0; y < hashSize; y++ {
+		bits[y] = make([]bool, hashSize)
+		for x := 0; x < hashSize; x++ {
+			bits[y][x] = float64(resized[y][x]) > avg
+		}
+	}
+	return newHashFromBits(bits), nil
+}
+
+func rgbToGray(rgb [][][3]uint8) [][]uint8 {
+	h := len(rgb)
+	w := len(rgb[0])
+	out := make([][]uint8, h)
+	for y := 0; y < h; y++ {
+		row := make([]uint8, w)
+		for x := 0; x < w; x++ {
+			row[x] = pilgray.ToGray(rgb[y][x][0], rgb[y][x][1], rgb[y][x][2])
+		}
+		out[y] = row
+	}
+	return out
+}
---
+++ hash/go/imagehash/group2_test.go
@@ -0,0 +1,29 @@
+package imagehash_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/wmetcalf/rosetta-image-hash/go/imagehash"
+	"github.com/wmetcalf/rosetta-image-hash/go/imagehash/internal/testkit"
+)
+
+func TestAverageHashGoldens(t *testing.T) {
+	cases, err := testkit.AlgorithmCasesFromRoot("average_hash")
+	if err != nil {
+		t.Fatalf("load goldens: %v", err)
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(fmt.Sprintf("%s-size-%d", c.Fixture, c.Size), func(t *testing.T) {
+			img := testkit.LoadPreDecodedFromRoot(t, c.Fixture)
+			h, err := imagehash.AverageHash(img, c.Size)
+			if err != nil {
+				t.Fatalf("AverageHash: %v", err)
+			}
+			if got := h.ToHex(); got != c.Hex {
+				t.Errorf("fixture=%s size=%d: got %q, want %q", c.Fixture, c.Size, got, c.Hex)
+			}
+		})
+	}
+}
