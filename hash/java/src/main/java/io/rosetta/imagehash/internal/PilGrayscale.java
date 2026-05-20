a76411912386ad1ce911d14a45555db5ab8e0d45
Will Metcalf <william.metcalf@gmail.com>
2026-05-20 00:14:40 -0500

    java: PilGrayscale kernel + Group-1 test (30 cases)

---
+++ hash/java/src/main/java/io/rosetta/imagehash/internal/PilGrayscale.java
@@ -0,0 +1,11 @@
+package io.rosetta.imagehash.internal;
+
+/** PIL 'L' (grayscale) conversion via ITU-R 601 fixed-point luma. */
+public final class PilGrayscale {
+    /** Returns grayscale value 0..255 for uint8 RGB input. */
+    public static int toGray(int r, int g, int b) {
+        return (r * 19595 + g * 38470 + b * 7471 + 32768) >>> 16;
+    }
+
+    private PilGrayscale() {}
+}
